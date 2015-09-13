4c4dba919fdb97ed69c8d829cc70222403eed323
peccu <peccul+github@gmail.com>
2015-09-13 20:38:10 +0900

    merge Folder and collect Folder

--- Folder.py
+++ Folder.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import bookmark
+# pretty print
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+import Bookmark
 
 def getPath(item):
   if 'path' in item:
     return item['path']
   return ''
 
+def collect_list(parentPath, current):
+  # このフォルダのパスと子供一覧をリストに追加
+  children = Bookmark.gatherUrlOrHash(current)
+  folder = {
+    'path': parentPath,
+    'children': children
+  }
+  outlist = [folder]
+  # 子フォルダから再帰的にパスと子供一覧を集める
+  child_list = filter(Bookmark.isFolder, Bookmark.getChildren(current))
+  def collect_child(child):
+    newPath = parentPath + '/' + Bookmark.getTitle(child)
+    children = collect_list(newPath, child)
+    outlist.extend(children)
+  map(collect_child, child_list)
+  return outlist
+
+def collect(root):
+  # フォルダの一覧を得る
+  list = collect_list('', root)
+  return list
