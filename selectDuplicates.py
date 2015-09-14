3fed7cee46de07c39beaa72a6e295167fd33f837
shimatani <shimatani@tgl.co.jp>
2015-09-14 12:53:32 +0900

    use Folder module method

--- selectDuplicates.py
+++ selectDuplicates.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import Folder
+
 def select_each(item):
   def select_print(path):
-    print str(len(item['path'])) + ': folder:' + item['path'] + ' => ' + path
+    print str(len(Folder.getPath(item))) + ': folder:' + Folder.getPath(item) + ' => ' + path
   map(select_print, item['dup'])
 
 def select_duplicate(list):
