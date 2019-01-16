afe52d633cb391f1b73804f85628369e87481fd6
peccu <peccul@gmail.com>
2019-01-17 07:17:41 +0900

    update selection

--- selectDuplicates.py
+++ selectDuplicates.py
@@ -4,7 +4,13 @@ import Folder
 
 def select_each(item):
   def select_print(path):
-    print str(Folder.getDepth(item)) + ': folder:' + Folder.getPath(item) + ' => ' + path
+    # print str(Folder.getDepth(item)) + ': folder:' + Folder.getPath(item) + ' => ' + path
+    if item['path'] == path:
+      return
+    if (len(item['path']) < len(path)) or (len(item['path']) == len(path) and item['path'] < path):
+      print str(len(item['path'])) + ': folder:' + item['path'] + ' => ' + path
+    else:
+      print str(len(path)) + ': folder:' + path + ' => ' + item['path']
   map(select_print, item['dup'])
 
 def select_duplicate(list):
